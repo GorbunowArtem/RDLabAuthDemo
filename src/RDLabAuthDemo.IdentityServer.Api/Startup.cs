bcef6b0c3e71ebc602d6eb4c5b7a490b9742f1d1
Artem Horbunov <gorbunowartemwork@gmail.com>
2021-07-27 16:29:46 +0300

    Added IS projects

---
+++ src/RDLabAuthDemo.IdentityServer.Api/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace RDLabAuthDemo.IdentityServer.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "https://localhost:5002";
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiPolicy", policy =>
+                {
+                    policy.RequireAuthenticatedUser();
+                    // This scope set in `Config.cs` file RDLabAuthDemo.IdentityServer project
+                    policy.RequireClaim("scope", "rdLabApi");
+                });
+            });
+            
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RDLabAuthDemo.IdentityServer.Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RDLabAuthDemo.IdentityServer.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/RDLabAuthDemo.IdentityServer/Config.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4;
+
+namespace RDLabAuthDemo.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("rdLabApi"),
+                new ApiScope("rdLabApiCode"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new[]
+            {
+                // Client credentials flow client
+                // https://auth0.com/docs/flows/client-credentials-flow
+                new Client
+                {
+                    ClientId = "rdlab.client",
+                    ClientName = "RD Lab demo Client Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+
+                    AllowedScopes = { "rdLabApi" }
+                },
+
+                // interactive client using code flow + pkce
+                // https://auth0.com/docs/flows/authorization-code-flow-with-proof-key-for-code-exchange-pkce
+                new Client
+                {
+                    ClientId = "rdlab.authcode.client",
+                    ClientSecrets = {new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"https://localhost:6002/signin-oidc"},
+                    FrontChannelLogoutUri = "https://localhost:6002/signout-oidc",
+                    PostLogoutRedirectUris = {"https://localhost:6002/signout-callback-oidc"},
+                    AllowOfflineAccess = true,
+                    RequirePkce = true,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "rdLabApiCode"
+                    }
+                },
+            };
+    }
+}
